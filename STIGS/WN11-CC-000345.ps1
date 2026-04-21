815f82e148b0a15fe22979e2c2c99e3063d88356
Maxim Aschenbrenner <aschenbrenner.work09@gmail.com>
2026-04-21 17:13:16 +0100

    Upload STIGS WN11-CC-000315 and WN11-CC-000345

---
+++ STIGS/WN11-CC-000345.ps1
@@ -0,0 +1,41 @@
+ <#
+.SYNOPSIS
+    This PowerShell script disables basic authentication for WinRM.
+.NOTES
+    Author          : Maxim Aschenbrenner
+    LinkedIn        : linkedin.com/in/aschenbrenner-maxim
+    GitHub          : github.com/maximaschenbrenner
+    Date Created    : 2026-04-21
+    Last Modified   : 2026-04-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000345
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000345.ps1 
+#>
+
+# Define the registry path and value
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service\"
+$valueName = "AllowBasic"
+$valueData = 0  # 0x00000000 in hexadecimal
+
+# Check if the registry path exists, if not create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the AlwaysInstallElevated value
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+# Output success message
+Write-Host "Registry value '$valueName' set to '$valueData' at '$registryPath'."
