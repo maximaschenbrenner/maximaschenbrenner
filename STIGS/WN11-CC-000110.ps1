c512e0aff15d0fb761a15ab5bfa064adfe125390
Maxim Aschenbrenner <aschenbrenner.work09@gmail.com>
2026-04-21 14:16:37 +0100

    Upload STIGS WN11-CC-000326 and WN11-CC-000110

---
+++ STIGS/WN11-CC-000110.ps1
@@ -0,0 +1,42 @@
+ <#
+.SYNOPSIS
+    This PowerShell script disables printing over HTTP.
+
+.NOTES
+    Author          : Maxim Aschenbrenner
+    LinkedIn        : linkedin.com/in/aschenbrenner-maxim
+    GitHub          : github.com/maximaschenbrenner
+    Date Created    : 2026-04-21
+    Last Modified   : 2026-04-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000110
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000110.ps1 
+#>
+
+# Define the registry path and value
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\"
+$valueName = "DisableHTTPPrinting"
+$valueData = 1  # 0x00000001 in hexadecimal
+
+# Check if the registry path exists, if not create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the DisableHTTPPrinting value
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+# Output success message
+Write-Host "Registry value '$valueName' set to '$valueData' at '$registryPath'."
